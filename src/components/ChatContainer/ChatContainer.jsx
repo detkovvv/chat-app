77b012d60166519eb4bc9b8d77d6eeb19d72a8c7
Detkov Vasiliy <fgh.detkov@gmail.com>
2023-05-26 15:02:46 +0300

    refactoring

--- src/components/ChatContainer/ChatContainer.jsx
+++ src/components/ChatContainer/ChatContainer.jsx
@@ -4,6 +4,7 @@ import { ChatMessage } from '../ChatMessage/ChatMessage.jsx';
 import { instance } from '../../helpers/axios/index.js';
 import { getApiLink } from '../../helpers/getApiLink.js';
 
+// eslint-disable-next-line react/prop-types
 export const ChatContainer = ({ user, chatMessages }) => {
     const [value, setValue] = useState('');
     const [messages, setMessages] = useState(chatMessages ? chatMessages : []);
@@ -14,56 +15,46 @@ export const ChatContainer = ({ user, chatMessages }) => {
         setValue(event.target.value);
     };
 
-    const sendMessage = async (event) => {
-        event.preventDefault();
-        await instance
-            .post(getApiLink('sendMessage'), {
-                chatId: `${user}@c.us`,
-                message: `${value}`,
-            })
-            .then((response) => {
-                setMessages([
-                    ...messages,
-                    {
-                        type: 'outgoing',
-                        idMessage: response.data.idMessage,
-                        timestamp: Date.now(),
-                        textMessage: `${value}`,
-                    },
-                ]);
-            });
+    const sendMessage = async () => {
+        const response = await instance.post(getApiLink('sendMessage'), {
+            chatId: `${user}@c.us`,
+            message: `${value}`,
+        });
+        setMessages([
+            ...messages,
+            {
+                type: 'outgoing',
+                idMessage: response.data.idMessage,
+                timestamp: Date.now(),
+                textMessage: `${value}`,
+            },
+        ]);
         setValue('');
         console.log(messages);
     };
 
     const getMessage = async () => {
-        await instance
-            .get(getApiLink('ReceiveNotification'))
-            .then((response) => {
-                if (response.data != null) {
-                    if (response.data.body.senderData.sender === `${user}@c.us`) {
-                        setMessages([
-                            ...messages,
-                            {
-                                type: 'incoming',
-                                idMessage: response.data.body.idMessage,
-                                timestamp: response.data.body.timestamp,
-                                textMessage:
-                                    response.data.body.messageData.textMessageData.textMessage,
-                            },
-                        ]);
-                    }
-                    instance
-                        .delete(getApiLink('deleteNotification', response.data.receiptId))
-                        .then((response) => console.log(response.data))
-                        .catch((error) => {
-                            console.log(error);
-                        });
-                }
-            })
-            .catch(() => {
-                console.log('нет входящих сообщений');
-            });
+        const response = await instance.get(getApiLink('ReceiveNotification')).catch(() => {
+            console.log('нет входящих сообщений');
+        });
+        if (response.data != null) {
+            if (response.data.body.senderData.sender === `${user}@c.us`) {
+                setMessages([
+                    ...messages,
+                    {
+                        type: 'incoming',
+                        idMessage: response.data.body.idMessage,
+                        timestamp: response.data.body.timestamp,
+                        textMessage: response.data.body.messageData.textMessageData.textMessage,
+                    },
+                ]);
+            }
+            instance
+                .delete(getApiLink('deleteNotification', response.data.receiptId))
+                .catch((error) => {
+                    console.log(error);
+                });
+        }
     };
 
     useEffect(() => {
